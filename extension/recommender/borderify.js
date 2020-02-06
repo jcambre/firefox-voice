e6ca06259f22bea4b689b47940688678ef0f082f
Julia <jcambre@alumni.stanford.edu>
2020-02-06 05:20:45 -0500

    Make recommendation popup dismissable

--- extension/recommender/borderify.js
+++ extension/recommender/borderify.js
@@ -1,10 +1,12 @@
-console.log("I AM HERE");
 browser.runtime.onMessage.addListener(message => {
     if (message.type === "onRecommendablePage") {        
         const span = document.createElement("div");
-        span.innerHTML = `Try asking Firefox Voice...<br/><b>"${message.recommendation}"</b>`;
+        span.innerHTML = `<div id="recommendation-dismiss">X</div><div id='recommendation-text'>Try asking Firefox Voice...<br/><b>"${message.recommendation}"</b></div>`;
         span.id = "recommendation-popup"
 
         document.body.prepend(span);
+        document.getElementById('recommendation-dismiss').addEventListener('click', function (event) {
+            document.getElementById('recommendation-popup').classList += "dismissed";
+        });
     }
 });
--- extension/recommender/recommender.js
+++ extension/recommender/recommender.js
@@ -7,10 +7,6 @@ const filter = {
 async function handleUpdated(tabId, changeInfo, tabInfo) {
     // Only report once
     if (tabInfo.status == "complete" && changeInfo.status) {
-        console.log("HEEERE");
-        console.log(`Updated tab: ${tabId}`);
-        console.log("Changed attributes: ", changeInfo);
-        console.log("New tab Info: ", tabInfo);
         if (tabInfo.isArticle) {
             await browser.tabs.insertCSS(tabId, {file: "recommender/recommender.css"});
             await browser.tabs.sendMessage(tabId, {type: "onRecommendablePage", recommendation: "Read this page"});            
