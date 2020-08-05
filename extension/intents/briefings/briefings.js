0e5550592003e8b2a8bf7c9ab41da75019560a87
Julia Cambre <jcambre@alumni.stanford.edu>
2020-08-05 18:21:17 -0400

    reduced functionality to simple radio playback

--- extension/intents/briefings/briefings.js
+++ extension/intents/briefings/briefings.js
@@ -1,25 +1,10 @@
 import * as intentRunner from "../../background/intentRunner.js";
-import * as browserUtil from "../../browserUtil.js";
-import * as content from "../../background/content.js";
 
 intentRunner.registerIntent({
   name: "briefings.news",
   async run(context) {
     const tab = await browser.tabs.create({
-      url: "https://www.bbc.co.uk/sounds/brand/p05hh4qy",
-    });
-
-    const firstEpisodeSelector = ".sc-c-playable-list-card__link";
-
-    await browserUtil.waitForPageToLoadUsingSelector(tab.id, {
-      selector: firstEpisodeSelector,
-    });
-    await content.inject(tab.id, [
-      "/intents/briefings/playFirstEpisode.content.js",
-    ]);
-    const found = await browser.tabs.sendMessage(tab.id, {
-      type: "playFirstEpisode",
-      selector: firstEpisodeSelector,
+      url: "https://www.bbc.co.uk/sounds/play/live:bbc_radio_one",
     });
   }
 });
--- extension/intents/briefings/playFirstEpisode.content.js
+++
@@ -1,7 +0,0 @@
-import { registerHandler } from "../../communicate.js";
-
-registerHandler("playFirstEpisode", message => {
-  const { selector } = message;
-  const firstEpisodeElement = document.querySelector(selector);
-  firstEpisodeElement.click();
-});
